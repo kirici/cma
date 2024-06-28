5a5a4af2812dd339c66061122601ff8e5a42e441
kirici <74684021+kirici@users.noreply.github.com>
2024-06-29 00:02:30 +0200

    feat: add slog-gin middleware, additional file output for logs

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,16 +1,41 @@
 package server
 
 import (
+	"io"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	sloggin "github.com/samber/slog-gin"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
-	r := gin.Default()
+	logFile := os.Getenv("LOG_FILE")
+	if logFile == "" {
+		logFile = "/tmp/cma.log.json"
+	}
+	f, err := os.OpenFile(logFile, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0o666)
+	if err != nil {
+		slog.Error("could not open log file, logging to stdout only", "err", err)
+	}
+	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(os.Stdout, f), nil))
+	config := sloggin.Config{
+		WithUserAgent:      false,
+		WithRequestID:      true,
+		WithRequestBody:    true,
+		WithRequestHeader:  false,
+		WithResponseBody:   true,
+		WithResponseHeader: true,
+		WithSpanID:         true,
+		WithTraceID:        true,
+	}
+
+	r := gin.New()
+	r.Use(sloggin.NewWithConfig(logger, config))
+	r.Use(gin.Recovery())
 
 	r.GET("/", s.HelloWorldHandler)
-
 	r.GET("/health", s.healthHandler)
 
 	return r
